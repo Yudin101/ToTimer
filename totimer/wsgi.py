7684575775e6d5e7d1bdd3985e8efd6494831c8c
Yudin Karki <yudinkarki00@gmail.com>
2024-07-30 23:03:30 +0545

    Change project's name

--- totimer/wsgi.py
+++ totimer/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pomodoro_timer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'totimer.settings')
 
 application = get_wsgi_application()
