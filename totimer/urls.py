3d34ea52c06f86d5bcd69c026ca84b6e3629283d
Yudin101 <yudinkarki00@gmail.com>
2024-10-02 12:48:45 +0545

    Project name change fix #2

--- totimer/urls.py
+++ totimer/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for pomodoro_timer project.
+URL configuration for totimer project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
