b1d2b8afb287221fda3a56dfecbbc9acdaab7689
Yudin101 <yudinkarki00@gmail.com>
2024-10-02 12:47:27 +0545

    Project name change fix

--- totimer/asgi.py
+++ totimer/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for pomodoro_timer project.
+ASGI config for totimer project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pomodoro_timer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'totimer.settings')
 
 application = get_asgi_application()
